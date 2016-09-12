a42a914ad5b1d7ded636cbc5e54776a44ac204b2
peterbynum <peterbynum@hanovernorwichschools.org>
2016-09-12 13:42:52 -0400

    Updated from Brython Server: September 12, 2016 1:42:52 PM EDT

--- cryptography.py
+++ cryptography.py
@@ -7,6 +7,22 @@ Assignment:
 
 Write and submit a program that encrypts and decrypts user data.
 
+Enter e to encrypt, d to decrypt, or q to quit: z
+Did not understand command, try again.
+Enter e to encrypt, d to decrypt, or q to quit: e
+Message: Two plus two = Five
+Key: Lorem ipsum
++KF;B(CH=NIZ}m;R\Dt
+Enter e to encrypt, d to decrypt, or q to quit: d
+Message: +KF;B(CH=NIZ}m;R\Dt
+Key: Lorem ipsum
+Two plus two = Five
+Enter e to encrypt, d to decrypt, or q to quit: q
+Goodbye!
+
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
+
+
+
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
